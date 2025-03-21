86d108111e39786309c1e3595fbb28ffe22fdf17
AzukiPL <maciejwojtkiewicz101@wp.pl>
2025-03-21 18:25:22 +0100

    tlo

--- head/QuizScreen.hpp
+++ head/QuizScreen.hpp
@@ -7,10 +7,17 @@ namespace hackathon
 	class QuizScreen : public Screen
 	{
 		public:
-			QuizScreen(raylib::Window& window) : Screen(window){};
+			QuizScreen(raylib::Window& window) :
+				Screen(window), 
+				m_background(LoadTexture("../graphic/pytanie.jpg")){};
+			
 			void compute() override {};
-			void display() override {};
+			void display() override {
+				DrawTexture(m_background,0,0,WHITE);
+				
+			};
 		private:
+			Texture2D m_background;
 			
 		
 	};
