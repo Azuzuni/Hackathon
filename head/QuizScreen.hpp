204853c0e9f12bcb9072146e40400e684dd1137d
AzukiPL <maciejwojtkiewicz101@wp.pl>
2025-03-21 22:47:33 +0100

    Update QuizScreen.hpp

--- head/QuizScreen.hpp
+++ head/QuizScreen.hpp
@@ -24,28 +24,31 @@ namespace hackathon
 				const int screenWidth = m_window.GetWidth();
 				const int screenHeight = m_window.GetHeight();
 
-				DrawButton(70,250, 9,0);
-				DrawButton(950,250, 9,1);
-				DrawButton(70,630, 9,2);
-				DrawButton(950,630, 9,3);
+				DrawButton(70,250, m_index,0);
+				DrawButton(950,250, m_index,1);
+				DrawButton(70,630, m_index,2);
+				DrawButton(950,630, m_index,3);
 			};
 		private:
 			Texture2D m_background;
 			Texture2D m_buttonTexture;
-			int m_index{0};
+			int m_index = {0};
 			std::vector<QuestionData> m_questions;
 			
 		private:
-			const void DrawButton(const int x, const int y, const int questionIndex,const int answerIndex) const {
+			void DrawButton(const int x, const int y, const int questionIndex,const int answerIndex) {
 				Vector2 mousePosition = GetMousePosition();
-				Rectangle rec = {x,y,static_cast<double>(m_buttonTexture.width),static_cast<double>(m_buttonTexture.height)};
+				Rectangle rec = {x,y,m_buttonTexture.width,m_buttonTexture.height};
 				bool isHovered = CheckCollisionPointRec(mousePosition,rec);
 				if(!isHovered) DrawTexture(m_buttonTexture,x,y,WHITE);
-				else DrawTexture(m_buttonTexture,x,y,GRAY);
+				else {
+					DrawTexture(m_buttonTexture,x,y,GRAY);
+					if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) ++m_index;
+				}
+				
 				DrawText(m_questions[questionIndex].answers[answerIndex].c_str(),x+(m_buttonTexture.width/10),y+(m_buttonTexture.height/3),30,BLACK);
 
 			}			
-		
 	};
 	
 }
