e815d5158e33503755d2b0c3f60dc69544126d97
Sathaniel7 <94134245+Sathaniel7@users.noreply.github.com>
2025-03-21 17:19:58 +0100

    screen manager w mainie

--- head/ScreenManager.hpp
+++ head/ScreenManager.hpp
@@ -1,9 +1,14 @@
-
+#include "raylib/raylib-cpp.hpp"
 
 namespace hackathon
 {
 	class ScreenManager
 	{
-
+	public:
+		ScreenManager(raylib::Window&);
+		void compute();
+		void display();
+	private:
+		raylib::Window& m_window;
 	};
 }
---
+++ src/ScreenManager.cpp
@@ -0,0 +1,19 @@
+#include "ScreenManager.hpp"
+
+namespace hackathon
+{
+	ScreenManager::ScreenManager(raylib::Window& window) : m_window(window)
+	{
+
+	}
+
+	void ScreenManager::compute()
+	{
+
+	}
+
+	void ScreenManager::display()
+	{
+
+	}
+}
